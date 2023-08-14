fda30b50f251e5d12f6fe61f8ea4dc54ae6091bb
23solo <yagami.naruto23@gmail.com>
2023-08-14 17:04:28 +0530

    [FEAT] create user with necessary requirements

--- Backend/dist/Main.js
+++ Backend/dist/Main.js
@@ -2,3 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const initialize_1 = require("./ChessBoard/initialize");
 const board = (0, initialize_1.initializeBoard)();
+const user1 = {
+    name: 'Solo',
+    color: 'W',
+    canCastleLeft: true,
+    canCastleRight: true,
+    isKingInCheck: false,
+};
+const user2 = {
+    name: 'Solo',
+    color: 'B',
+    canCastleLeft: true,
+    canCastleRight: true,
+    isKingInCheck: false,
+};
---
+++ Backend/src/user/createUser.ts
@@ -0,0 +1,2 @@
+import { User } from './User';
+// Create user on runTime laterOn
